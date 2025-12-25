f0fc3920d8fe875dcf8760c88907c4b3eca0d8ea
Ruturaj Ghodke <59768319+shotcaller@users.noreply.github.com>
2025-12-25 16:15:50 -0400

    Feature/new code (#12)
    * set up node and lint
    
    * Revert "set up node and lint"
    
    This reverts commit 0fdfcf74cc9640bf583420cc6a81d3571046b8d8.
    
    * code files with jest
    
    * updated eslint config
    
    * updated eslint config to add globals
    
    * updated eslint config to add globals typo
    
    ---------
    
    Co-authored-by: Ruturaj Ghodke <shotcaller@github.com>

---
+++ eslint.config.cjs
@@ -0,0 +1,23 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "commonjs",
+      globals: globals.node,
+    },
+    rules: {
+      "no-unused-vars": "error",
+    },
+  },
+
+  {
+	files: ["**/*.test.js", "**/*.spec.js"],
+	languageOptions: {
+		globals: globals.jest,
+},
+}
+];
